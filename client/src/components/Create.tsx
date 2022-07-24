a994b11065b8ca0406f34c71fe01f09af0906640
Iwan Traeger-Payne <iwantraegerpayne@gmail.com>
2022-07-24 12:02:31 -0400

    added part of create task from

--- client/src/components/Create.tsx
+++ client/src/components/Create.tsx
@@ -1,17 +1,68 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type Props = {};
 
+interface CardData {
+  name: string;
+  info: string;
+}
+
 const Create = ({}: Props) => {
-  const [collapsed, setCollapsed] = useState(true);
+  const [data, setData] = useState<CardData>({
+    name: "",
+    info: "",
+  });
+  const [collapsed, setCollapsed] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("/api/v1/tasks", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: data.name }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setData((data) => {
+      return {
+        ...data,
+        [name]: value,
+      };
+    });
+  };
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
   return (
-    <div
-      className={`create  ${collapsed ? "collapsed" : ""}`}
-      onClick={() => setCollapsed(!collapsed)}
-    >
-      Create
+    <div className={`create-task`} onClick={() => setCollapsed(!collapsed)}>
+      <div className="create-task__header">
+        <h2 className="create-task__header__title">create a new task</h2>
+      </div>
+      <form className="create-task__form" onSubmit={handleSubmit}>
+        <label>
+          Task Name:
+          <input
+            type="text"
+            value={data.name}
+            name="name"
+            onChange={handleChange}
+          />
+        </label>
+        <button>add task</button>
+      </form>
     </div>
   );
 };
 
 export default Create;
+
+// ${collapsed ? "collapsed" : ""} code to collapse card
